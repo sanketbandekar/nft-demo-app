77e5b13ae934859ef9ad05e6036fe381557b2474
sanket <bandekarsanket03@gmail.com>
2022-07-01 14:05:28 +0530

    NFT card implemented

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -2,16 +2,36 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { SIZES, FONTS, COLORS, SHADOWS, assets } from '../constants'
 
-export const NFTTitle = () => {
+export const NFTTitle = ({ title, subTitle, titleSize, subTitleSize }) => {
   return (
-    <Text>NFTTitle</Text>
+    <View>
+      <Text style={{
+        fontFamily: FONTS.semiBold, fontSize: titleSize,
+        color: COLORS.primary
+      }}>{title}</Text>
+
+      <Text style={{
+        fontFamily: FONTS.regular, fontSize: subTitleSize,
+        color: COLORS.primary
+      }}>{subTitle}</Text>
+    </View>
+    
   )
 }
 
-export const EthPrice = () => {
+export const EthPrice = ({price}) => {
     return (
-      <View>
-        <Text>EthPrice</Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image
+          source={assets.eth}
+          resizeMode="contain"
+          style={{ width: 20, height: 20, marginRight: 2 }}
+        />
+        <Text style={{
+          fontFamily: FONTS.medium,
+          fontSize: SIZES.font,
+          color: COLORS.primary,
+        }}>{price}</Text>
       </View>
     )
 }
@@ -43,8 +63,32 @@ export const People = () => {
 
 export const EndDate = () => {
     return (
-      <View>
-        <Text>EndDate</Text>
+      <View
+      style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: '50%'
+      }}>
+        <Text style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.primary
+        }}>
+        Ending In
+        </Text>
+
+        <Text style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.medium,
+          color: COLORS.primary
+        }}>
+        12h 30m
+        </Text>
       </View>
     )
 }
