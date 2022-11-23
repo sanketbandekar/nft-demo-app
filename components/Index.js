20f425831972b72a5aee69242fa55f7f91a9090a
sanket <bandekarsanket03@gmail.com>
2022-11-23 18:23:34 +0530

    Details page and search content

--- components/Index.js
+++ components/Index.js
@@ -4,8 +4,10 @@ import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
 import DetailsDesc from "./DetailsDesc";
 import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
 export {
     HomeHeader, NFTCard, FocusedStatusBar,
-    CircleButton, RectButton, DetailsBid, DetailsDesc
+    CircleButton, RectButton, DetailsBid, DetailsDesc,
+    SubInfo
 }
--- screens/Details.js
+++ screens/Details.js
@@ -1,11 +1,36 @@
+import React from 'react'
 import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
 import { COLORS, SIZES, SHADOWS, FONTS, assets } from '../constants'
 import { CircleButton, RectButton, SubInfo, FocusedStatusBar,
 DetailsDesc, DetailsBid } from '../components/Index'
-import React from 'react'
+
+const DetailsHeader = ({ data,navigation }) => (
+  <View style={{ width: '100%', height: 373 }}>
+    <Image
+      source={data.image}
+      resizeMode="cover"
+      style={{ width: '100%', height:'100%' }}
+    />
+
+    <CircleButton
+      imgUrl={assets.left}
+      handlePress={ () => navigation.goBack() }
+      left={15}
+      top={StatusBar.currentHeight + 10}
+    />
+
+    <CircleButton
+      imgUrl={assets.heart}
+      right={15}
+      top={StatusBar.currentHeight + 10}
+    />
+  </View>
+)
+
 
 const Details = ({ route, navigation }) => {
   const { data } = route.params;
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar
@@ -35,7 +60,22 @@ const Details = ({ route, navigation }) => {
         contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
         ListHeaderComponent={ () => (
           <React.Fragment>
-              <DetailsHeader />
+              <DetailsHeader data={data} navigation={navigation} />
+              <SubInfo/>
+              <View style={{padding: SIZES.font}}>
+                <DetailsDesc data={data}/>
+
+                {data.bids.length > 0 && (
+                  <Text style={{
+                    fontSize: SIZES.font,
+                    fontFamily: FONTS.semiBold,
+                    color: COLORS.primary
+                  }}>
+                    Current Bids
+                  </Text>
+                )}
+
+              </View>
           </React.Fragment>
         ) }
       />
